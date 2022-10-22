6908517425fa4ca9ed541fa2248647e16a250708
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-10-22 11:15:54 +0000

    Adding tarfile member sanitization to extractall()

--- active-learning-workshop/text_featurization/lm_finetune/load_data.py
+++ active-learning-workshop/text_featurization/lm_finetune/load_data.py
@@ -101,7 +101,26 @@ def load_imdb_data(data_dir:str=data_dir, out_numpy:bool=False):
             print(50 * '=')
             print("Extracting tarfile")
             with tarfile.open(tar_file, 'r:gz') as tar:
-                tar.extractall(path = target_dir)
+                def is_within_directory(directory, target):
+                    
+                    abs_directory = os.path.abspath(directory)
+                    abs_target = os.path.abspath(target)
+                
+                    prefix = os.path.commonprefix([abs_directory, abs_target])
+                    
+                    return prefix == abs_directory
+                
+                def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+                
+                    for member in tar.getmembers():
+                        member_path = os.path.join(path, member.name)
+                        if not is_within_directory(path, member_path):
+                            raise Exception("Attempted Path Traversal in Tar File")
+                
+                    tar.extractall(path, members, numeric_owner=numeric_owner) 
+                    
+                
+                safe_extract(tar, path=target_dir)
                 
         print(50 * "-")
         print("CSV not found, loading from source, this might take a few minutes")
